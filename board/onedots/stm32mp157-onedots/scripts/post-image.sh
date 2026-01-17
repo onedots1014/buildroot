f918eee2509794ea03a0eb1f50758c67f51e95a4
onedots <onedots0@outlook.com>
2026-01-17 11:58:23 +0800

    udate post-image.sh

--- board/onedots/stm32mp157-onedots/scripts/post-image.sh
+++ board/onedots/stm32mp157-onedots/scripts/post-image.sh
@@ -1,31 +1,9 @@
-# #!/bin/bash
-# set -e
-
-# BOARD_DIR="$(dirname $0)"
-# OVERLAY_BOOT_DIR="$BOARD_DIR/../overlay/boot"
-
-# # Create BINARIES_DIR if it doesn't exist
-# if [ ! -d "${BINARIES_DIR}" ]; then
-#     echo "Creating BINARIES_DIR at ${BINARIES_DIR}"
-#     mkdir -p "${BINARIES_DIR}"
-# fi
-
-# if [ ! -d "$OVERLAY_BOOT_DIR" ]; then
-#     echo "Error: Overlay boot directory not found at $OVERLAY_BOOT_DIR"
-#     exit 1
-# fi
-
-# echo "Copying overlay boot files to ${BINARIES_DIR}"
-# cp -rv "$OVERLAY_BOOT_DIR"/* "${BINARIES_DIR}/"
-
-# exit $?
-
-
 #!/bin/bash
 set -e
 
 BOARD_DIR="$(dirname "$0")"
 OVERLAY_BOOT_DIR="$BOARD_DIR/../overlay/boot"
+OVERLAY_TFA_FILE="$BOARD_DIR/../overlay/tf-a-onedots_stm32mp157c-512d-v1-serialboot.stm32"
 : "${BINARIES_DIR:=output/images}"
 
 # 创建 BINARIES_DIR
@@ -33,6 +11,7 @@ mkdir -p "$BINARIES_DIR"
 
 # 复制 boot overlay
 cp -rv "$OVERLAY_BOOT_DIR"/* "$BINARIES_DIR/"
+cp -rv "$OVERLAY_TFA_FILE" "$BINARIES_DIR/"
 
 # 调用官方 genimage.sh
 support/scripts/genimage.sh -c "$BOARD_DIR/genimage-tfa.cfg"
